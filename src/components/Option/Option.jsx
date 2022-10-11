7f244f3af32f6fe407dba1f3719798f44b4d0704
Oliulla <razasm29@gmail.com>
2022-10-11 14:46:03 +0600

    added show quiz correct answer

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -1,37 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './Option.css'
 
 const Option = ({option, quizQuestion}) => {
     const {correctAnswer} = quizQuestion
-    const {findCorrect, setFindCorrect} = useState(false);
     
     const handleFindCorrect = (clickedOption) => {
-        // console.log(clickedOption, '||',  correctAnswer)
         const correctOption = correctAnswer.split(' ').join('');
         const userClickedOption = clickedOption.split(' ').join('');
-        // console.log(correctAnswer.split(' ').join(''))
-        // console.log(clickedOption.split(' ').join(''))
-        // const dataChk = clickedOption === correctAnswer;
+        
         if(userClickedOption === correctOption) {
-            console.log('correct answere')
-            toast.success("Correct Answere!", {autoClose: 600});
+            toast.success("Correct Answere!", {autoClose: 900});
         }
         else {
-            toast.warning('Incorrect Answere', {autoClose: 600});
-            
+            toast.warning('Incorrect Answere', {autoClose: 900});
         }
-        // setFindCorrect('False');
-        // console.log("Not correct")
     }
-    // console.log(findCorrect);
 
     return (
         <div 
             onClick={(e) => handleFindCorrect(e.target.innerText)}
             className={`listSquare border border-emerald-500 mt-8 text-2xl w-11/12 md:w-8/12 
-            px-2 py-4 cursor-pointer  `}
+            px-2 py-4 cursor-pointer`}
         >
             <p>{option}</p>
         </div>
--- src/components/QuizQuestionsDetails/QuizQuestionsDetails.jsx
+++ src/components/QuizQuestionsDetails/QuizQuestionsDetails.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Option from '../Option/Option';
 
 const QuizQuestionsDetails = ({quizQuestion, index}) => {
-    // console.log(quizQuestion);
-    const {question, options} = quizQuestion;
+    const {question, correctAnswer, options} = quizQuestion;
+    const [correct, setCorrect] = useState();
+
+    const handleShowCorrectAnswere = () => {
+        setCorrect(correctAnswer)
+    }
+    console.log(correct)
+
     return (
         <div className='flex flex-col items-start my-14'>
             <ol>
                 <li className='text-3xl'>Q-{index+1}: {question.slice(3, -4)}</li>
+                <button 
+                    onClick={handleShowCorrectAnswere}
+                    className='bg-blue-900 mt-2 p-2 rounded-md'
+                >
+                    Show Correct Answer
+                </button>
+                <p className={`bg-gray-700 md:w-9/12 p-4 rounded-md mt-2 md:text-xl ${correct ? 'block' : 'hidden'}`}>{correct}</p>
             </ol>
             {
                 options.map(option => 
