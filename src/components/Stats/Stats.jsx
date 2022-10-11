c7ed9af194687cdf62ef9e39dff6b8b4d551f644
Oliulla <razasm29@gmail.com>
2022-10-11 22:56:09 +0600

    added linechart

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -8,15 +8,53 @@ import {
     CartesianGrid,
     Tooltip,
     Legend,
-    ResponsiveContainer
+    ResponsiveContainer,
+    LineChart,
+    Line
   } from "recharts";
+
+
+  const assignments = [
+    {
+      assignment: 'Assignment 1',
+      marks: 60
+    },
+    {
+      assignment: 'Assignment 2',
+      marks: 60
+    },
+    {
+      assignment: 'Assignment 3',
+      marks: 60
+    },
+    {
+      assignment: 'Assignment 4',
+      marks: 60
+    },
+    {
+      assignment: 'Assignment 5',
+      marks: 60
+    },
+    {
+      assignment: 'Assignment 6',
+      marks: 60
+    },
+    {
+      assignment: 'Assignment 7',
+      marks: 60
+    },
+    {
+      assignment: 'Assignment 8',
+      marks: 60
+    }
+];
 
 
 const Stats = () => {
     const data = useContext(QuizContext);
 
     return (
-        <div className='w-11/12 flex justify-center md:w-9/12 mx-auto my-10'>
+        <div className='w-11/12 flex flex-col items-center md:w-9/12 mx-auto my-10'>
             <ResponsiveContainer width="95%" height={400}>
                 <BarChart
                     width={500}
@@ -37,7 +75,29 @@ const Stats = () => {
                     <Legend />
                     <Bar dataKey="total" fill="#82ca9d" />
                 </BarChart>
-                
+            </ResponsiveContainer>
+            <div className='mt-32 mb-6 text-2xl'>
+                <h3>Assignments Marks</h3>
+            </div>
+            <ResponsiveContainer width="95%" height={400}>
+                <LineChart
+                width={500}
+                height={300}
+                data={assignments}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="assignment" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="marks" stroke="#8884d8" activeDot={{ r: 8 }} />
+                </LineChart>
             </ResponsiveContainer>
         </div>
 
